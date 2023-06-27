5bc2cf5cd53ae44b4d79d1d4088da602d774b9c6
Coder-Nicki <nicki5389@gmail.com>
2023-06-27 14:53:16 +1000

    added the year

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Nicole Hulett's portfolio website coming soon...</h1>
+      <h2>2023</h2>
     </div>
   );
 }
